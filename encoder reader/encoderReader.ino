423ac698f7e84ae6b1c80db419af1734b795a7e6
shadowlerone <andrae.lewis@gmail.com>
2021-04-15 19:24:22 -0400

    make motor spin

--- encoder reader/encoderReader.ino
+++ encoder reader/encoderReader.ino
@@ -13,10 +13,15 @@ using namespace CrcLib;
 //   Low Performance:  neither pin has interrupt capability
 Encoder motor1Enco(CRC_ENCO_A, CRC_ENCO_B);
 
+bool motorRotating = LOW;
+const int STOP_POSITION = 1994;
+const int MOTOR_SPEED = 13; //127*0.10=13
 void setup()
 {
 	Serial.begin(9600);
 	Serial.println("Encoder Test:");
+	CrcLib::InitializePwmOutput(MC_1);
+	motor1Enco.write(0);
 }
 
 long positionLeft = 0;
@@ -27,6 +32,7 @@ void loop()
 	enc = motor1Enco.read();
 	Serial.print(enc);
 	Serial.println();
+	CrcLib::SetPwmOutput(MC_1,MOTOR_SPEED);
 	// if a character is sent from the serial monitor,
 	// reset both back to zero.
 	if (Serial.available())
